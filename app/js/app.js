4f5857b3ea2df264c50de60e7c62636553ad0059
bobby5develops <bobby5develops@gmail.com>
2017-06-26 17:09:02 -0400

    updated the footer css and wrote the helper methods for param fetching for input value check

--- app/js/app.js
+++ app/js/app.js
@@ -5,10 +5,22 @@ var purple = (function (window) {
 
     //vars
     var anchorEl = document.querySelector("a"),
-        targetEl = document.querySelectorAll('.hide')[0];
-        console.log(targetEl);
+        targetEl = document.querySelectorAll('.hide')[0],
+        offer = document.querySelectorAll('input[type="number"]')[0].value,
+        submitOffer = document.querySelectorAll('input[type="submit"]')[0];
 
 
+    //utility helpers
+    function between( x, min, max ){
+        return x >= min && x <= max;
+    }
+
+    function getUrlParameter(name) {
+        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+        var results = regex.exec(location.search);
+        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    }
 
 
     //event handlers
@@ -23,6 +35,51 @@ var purple = (function (window) {
         }
     };
 
+    //create function that validates the user input
+    var validateOffer = function(){
+        /**
+         * Get the value of a querystring
+         * @param  {String} field The field to get the value of
+         * @param  {String} url   The URL to get the value from (optional)
+         * @return {String}       The field value
+         */
+
+        //check the url param value
+        if (getUrlParameter(offer) < 189000){
+            console.log('Off too low.');
+            console.log(getUrlParameter(offer))
+            console.log(getUrlParameter('offerNum'));
+        }else {
+            console.log('Lets pop some champaign!');
+        }
+
+
+        console.log(getUrlParameter('offerNum'));
+
+
+
+
+
+
+
+       /* if( between( offer, 180000, 200000)){
+            console.log("GOOD PRICE!");
+        }else{
+            console.log("BAD PRICE!");
+        }
+
+        if(offer.length === 0 ){
+            console.log("Please enter a valid input!");
+        }else{
+
+            console.log(offer);
+        }
+
+        console.log("Saving value", offer);*/
+
+
+    };
+
     var ready = function () {
         console.log('html content fully loaded');
     };
@@ -30,11 +87,13 @@ var purple = (function (window) {
 
     //event listeners
     anchorEl.addEventListener("click", toggledItem, false);
+    submitOffer.addEventListener("click", validateOffer, false);
     document.addEventListener("DOMContentLoaded", ready);
 
     //api dispatcher
     return {
-        toggledItem: toggledItem
+        toggledItem: toggledItem,
+        validateOffer: validateOffer
     }
 }(window));
 
